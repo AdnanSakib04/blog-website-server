c3d5e0bf975cfe0648a541ba50636c498ede6ab2
AdnanSakib04 <navidadnan04@gmail.com>
2023-11-08 19:44:18 +0600

    post route for add comment

--- index.js
+++ index.js
@@ -131,94 +131,16 @@ async function run() {
     });
 
 
-    // ---------------------------------------------------------------------------------
-    //get route for brands
-    app.get('/brands', async (req, res) => {
-      const cursor = brandCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
+        //post route for add comment
+        app.post('/addComment', async (req, res) => {
+          const newComment = req.body;
+          console.log(newComment);
+          const result = await commentCollection.insertOne(newComment);
+          res.send(result);
+        })
 
 
-    //post route for add products
-    app.post('/products', async (req, res) => {
-      const newProduct = req.body;
-      console.log(newProduct);
-      const result = await productCollection.insertOne(newProduct);
-      res.send(result);
-    })
-
-
-    //get route for specific brand
-    app.get('/brands/:brand', async (req, res) => {
-      const brand = req.params.brand;
-      const query = { brand: brand }
-      const cursor = productCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-
-    // get route for product details
-    app.get('/products', async (req, res) => {
-      const cursor = productCollection.find();
-      const products = await cursor.toArray();
-      res.send(products);
-    })
-
-    //get route for update product
-    app.get('/products/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await productCollection.findOne(query);
-      res.send(result);
-    })
-
-    //put route for update products
-    app.put('/products/:id', async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) }
-      const options = { upsert: true };
-      const updatedProduct = req.body;
-
-      const product = {
-        $set: {
-          name: updatedProduct.name,
-          brand: updatedProduct.brand,
-          photo: updatedProduct.photo,
-          rating: updatedProduct.rating,
-          price: updatedProduct.price,
-          type: updatedProduct.type,
-        }
-      }
-
-      const result = await productCollection.updateOne(filter, product, options);
-      res.send(result);
-    })
-
-    //post route for product add to cart
-    app.post('/cart', async (req, res) => {
-      const cartProduct = req.body;
-      console.log(cartProduct);
-      const result = await cartCollection.insertOne(cartProduct);
-      res.send(result);
-    })
-
-    //get route for cart
-    app.get('/cart', async (req, res) => {
-      const cursor = cartCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-    //delete route for cart
-    app.delete('/cart/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await cartCollection.deleteOne(query);
-      res.send(result);
-    })
-    // ---------------------------------------------------------------------------------
+    
 
 
     // Send a ping to confirm a successful connection
